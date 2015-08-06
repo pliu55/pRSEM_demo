1b5148fede23931daa6e3e06af3daad5fd4ee141
Peng Liu <pliu55@wisc.edu>
2015-08-06 12:51:34 -0500

    add running script for preparing reference

--- run_pRSEM_demo.sh
+++ run_pRSEM_demo.sh
@@ -1,11 +1,7 @@
 #!/bin/bash 
 
-./scr/01_demo_setup.pl
+make
 
-./scr/02_rsem_prep_ref.pl
+./scr/01_prsem_prep_ref.pl
 
-./scr/03_rsem_calc_expr.pl
-
-./scr/04_prsem_tss_group.py
-
-./scr/05_prsem_ol_gene.py
+./scr/02_prsem_calc_expr.pl
